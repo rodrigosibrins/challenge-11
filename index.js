2ea6b49e8a9b5f1970159cd9f0a2f2db3afe4b3d
Rodri <rodrigosib2017@gmail.com>
2021-04-16 11:01:25 -0300

    routes updated

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 function handleRegister(req, res){
     console.log(req.body);
 }
 function handleLogin(req, res){}
 
-app.post('/register', (handleRegister));
+app.post('/register', handleRegister);
 
-app.put('/login', (handleLogin));
+app.put('/login', handleLogin);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
