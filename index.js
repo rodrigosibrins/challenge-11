91d3133a02791c171e6b07f69468d981c2979027
Rodri <rodrigosib2017@gmail.com>
2021-04-16 12:12:22 -0300

    cors added

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+
+
+app.use('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type, access_token');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 function handleRegister(req, res){
     console.log(req.body);
 }
+
 function handleLogin(req, res){}
 
 app.post('/register', handleRegister);
-
 app.put('/login', handleLogin);
 
 app.listen(port, () => {
